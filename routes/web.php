49350dcb0fe6e9a30f479bc7a8c89139d4c82aa1
MaeBzh <maelenn.picaud@gmail.com>
2017-10-24 19:45:11 +0200

    Ajout des routes pour l'Auth et vers ParentController

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,27 @@
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/tableau_de_bord', 'ParentController@index')->name('tableauDeBord');
+    Route::get('/nouvel_enfant', 'ParentController@showFormAddChild')->name('nouvelEnfant');
+    Route::post('/nouvel_enfant', 'ParentController@addChild')->name('nouvelEnfant');
+    Route::get('/enfant/{pseudo}','ParentController@chooseChild')->name('enfant');
 });
